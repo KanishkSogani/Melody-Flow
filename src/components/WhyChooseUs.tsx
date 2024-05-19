61bcf04a80fcf431c7ad0d4461a9ce7d0a3ec3c9
KanishkSogani <kanishksogani1@gmail.com>
2024-05-19 23:26:46 +0530

    Testimonials section created

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -9,16 +9,6 @@ const musicSchoolContent = [
       "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
   },
   {
-    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
-    description:
-      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
-  },
-  {
-    title: "Discover Your Sound with Us: A Personal Journey in Music Mastery",
-    description:
-      "Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.",
-  },
-  {
     title: "Live Feedback & Engagement",
     description:
       "Immerse yourself in an interactive learning experience where feedback is immediate, just like real-time changes in a collaborative project. This approach enhances your understanding and mastery of music concepts and performance techniques.",
