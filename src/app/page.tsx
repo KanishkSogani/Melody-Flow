ff9fee79dcc90786307f8e25c91e5427089aa965
KanishkSogani <kanishksogani1@gmail.com>
2024-05-19 22:17:50 +0530

    Why choose us component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className=" min-h-screen  bg-black/[0.96] antialiased bg-grid-white/[0.02">
       <HeroSection />
       <FeaturedSection />
+      <WhyChooseUs />
     </main>
   );
 }
