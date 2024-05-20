e1537824fe81514b72c3fdbad98aa1042c74cb59
KanishkSogani <kanishksogani1@gmail.com>
2024-05-20 18:52:33 +0530

    Webinar section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
 import Testimonials from "@/components/Testimonials";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <FeaturedSection />
       <WhyChooseUs />
       <Testimonials />
+      <UpcomingWebinars />
     </main>
   );
 }
