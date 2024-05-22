b830ea77585e303e72f40992bbd6ddd24694753e
KanishkSogani <kanishksogani1@gmail.com>
2024-05-23 00:53:45 +0530

    fixed responsiveness bug

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,18 +5,18 @@ import { AuroraBackground } from "../../components/ui/aurora-background";
 function page() {
   return (
     <AuroraBackground>
-      <div className=" z-10 flex flex-col justify-center items-center mx-8 mb-[10vh] min-w-[80vw]">
+      <div className=" z-10 flex flex-col justify-center items-center mx-8 ">
         <div>
           <h1 className="text-3xl md:text-5xl font-bold dark:text-white text-center">
             Contact Us
           </h1>
-          <p className="text-neutral-500 mt-5 max-w-lg mx-auto my-2 text-sm text-center text-[18px]">
+          <p className=" text-slate-300 mt-5 max-w-lg mx-auto my-2  text-center text-[16 px]">
             We&apos;re here to help with any questions about our courses,
             programs, or events. Reach out and let us know how we can assist you
             in your musical journey.
           </p>
         </div>
-        <div className="z-10 mt-4   ">
+        <div className="z-10 md:mt-4 md:pt-4   ">
           <form action="/" onSubmit={(e) => e.preventDefault()}>
             <div className="textInputWrapper min-w-[20rem] md:min-w-[35rem]">
               <input
@@ -32,7 +32,7 @@ function page() {
               />
             </div>
             <div className="flex justify-center items-center">
-              <button className="bg-black cursor-pointer dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+              <button className="bg-black cursor-pointer dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2 mt-4">
                 Submit Form
               </button>
             </div>
